5b83fc3019d79b6f2401666cf48f3029000ff028
Kouki Saito <dan.addr.skd@gmail.com>
2016-07-17 02:43:27 +0900

    Add Test

---
+++ test/test_test.py
@@ -0,0 +1,11 @@
+
+import unittest
+from app.for_test import ForTest
+
+class TestTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_test(self):
+        self.assertTrue(True)
+
