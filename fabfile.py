dc7e6130be8fafcfeb95a78b12e4112ff377870d
Kouki Saito <dan.addr.skd@gmail.com>
2016-07-20 03:10:13 +0900

    Add slack notification for deploy task

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,20 @@
 # It's for me, sorry
 
 from fabric.api import *
+import slackbot_settings as settings
+from urllib import request, parse
 
-env.hosts = [
-    "koukilab.com",
-]
+env.hosts = settings.DEPLOY_HOSTS
 
 def deploy():
+    slack("Deploy Started")
     with cd("/var/bot/slack-shogi"):
         run("git pull")
         run("supervisorctl reload")
+    slack("Deploy Finished")
+
+def slack(text):
+    if settings.WEBHOOK_URL:
+        payload = ("payload={\"text\": \""+parse.quote(text)+"\", \"username\": \"Mr.deploy\"}").encode("utf-8")
+        request.urlopen(url=settings.WEBHOOK_URL, data=payload)
 
