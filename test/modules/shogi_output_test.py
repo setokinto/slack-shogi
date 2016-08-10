17d94a0d827fd134a1284650f52538456ad2e884
yossy <j19.yk08343@gmail.com>
2016-08-10 23:40:51 +0900

    Remove test case for axis number mode.

--- test/modules/shogi_output_test.py
+++ test/modules/shogi_output_test.py
@@ -16,14 +16,6 @@ class ShogiTest(unittest.TestCase):
         board_str = ShogiOutput.make_board_emoji(board)
         self.assertIsNotNone(board_str)
 
-    def test_shogi_output_number_format(self):
-        # for syntax error and runtime error
-        channel_id = "channel_id"
-        shogi = ShogiInput.init(channel_id, [{"id": "user1", "name": "user1name"}, {"id": "user2", "name": "user2name"}])
-        board = ShogiInput.get_shogi_board(channel_id)
-        board_str = ShogiOutput.make_board_emoji(board, is_number=True)
-        self.assertIsNotNone(board_str)
-
     def test_shogi_output_reverse_format(self):
         # for syntax error and runtime error
         channel_id = "channel_id"
